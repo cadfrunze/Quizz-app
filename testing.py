78cfe9f21a8a4c80d4b2ca303fbd8b8526fe14f0
cadfrunze <marius1890@yahoo.com>
2023-06-18 16:22:53 +0300

    update

--- testing.py
+++ testing.py
@@ -17,3 +17,25 @@
 # with open('./work_log.txt', 'a') as fisier1:
 #     fisier1.writelines(lista[-1])
 
+from datetime import datetime
+
+minut = datetime.now().minute
+ora = datetime.now().hour
+zi = datetime.now().day
+luna = datetime.now().month
+an = datetime.now().year
+
+with open('./work_log.txt', 'r') as file:
+    lista = file.readlines()
+element: str = lista[-1]
+
+element = element.replace('12', f'{zi}')
+element = element.replace('6', f'{luna}')
+element = element.replace('2023', f'{an}')
+element = element.replace('20', f'{ora}')
+element = element.replace('30', f'{minut}')
+
+with open('./work_log.txt', 'a') as fisier:
+    fisier.writelines(element + '\n')
+
+
